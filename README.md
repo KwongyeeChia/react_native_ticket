ce1a7b4d41cfa36a8311410115242fae55122169
kwongyee <jiagy@chinamobiad.com>
2019-11-06 18:12:10 +0800

    添加下拉组件

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # react_native_ticket
-read
+1.装好环境
+2.下载夜神模拟器 
+  adb.exe connect 127.0.0.1:62001 
+  摇一摇 dev settings 设置本机端口号
+3.yarn android
+
--- src/scene/Movie/MovieScene.js
+++ src/scene/Movie/MovieScene.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, RefreshControl, ScrollView} from 'react-native';
 import CustomStatusBar from '../../widget/CustomStatusBar';
 
 class MovieScene extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRefresing: false,
+    };
+  }
   UNSAFE_componentWillMount() {
     // console.log('Home props:', this.props.navigation.state.params);
   }
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.isRefreshing}
+            onRefresh={() => this._onRefresh()}
+            colors={['#ff7887']}
+          />
+        }>
         <CustomStatusBar />
         <Button title="MovieScene" onPress={() => navigate('Per')} />
-      </View>
+      </ScrollView>
     );
+  }
+  _onRefresh() {
+    this.setState({isRefreshing: true});
+    setTimeout(() => {
+      this.setState({
+        isRefreshing: false,
+      });
+    }, 2000);
   }
 }
 export default MovieScene;
